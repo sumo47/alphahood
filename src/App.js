908d4550e143e85a09f489361e60da73307c6727
sumit kumar <sumokhan76342@gmail.com>
2023-04-29 20:36:05 +0530

    almost done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Login from './components/BootStrap';
 import Login2 from './components/Login';
 import LastLogin from './components/LastLogin'
 
 
 function App() {
+  const [mode, setMode] = useState("light")
+  const [head, setHead] = useState()
+  const [onboard , setOnboard] = useState()
+  const [input, setInput] = useState()
+  const [label, setLabel] = useState()
+  const [inputColor, setinputColor] = useState()
+  const [headColor, setheadColor] = useState()
+  const [headColor34, setheadColor34] = useState()
+
+  const Dark = () => {
+    if(mode === "light"){
+      setHead("#252D3C")
+      document.body.style.backgroundColor = '#151B28'
+      setOnboard("#151B28")
+      setInput("#1C2534")
+      setLabel("white")
+      setinputColor("#333B48")
+      setheadColor("#437EF7")
+      setheadColor34("#A5ACBA")
+      setMode("Dark")
+      
+    }else{
+      setHead("#FAFBFC")
+      setOnboard("#FAFBFC")
+      document.body.style.backgroundColor = '#FAFBFC'
+      setLabel("#272D37")
+      setinputColor("white")
+      setheadColor("#5F6D7E")
+      setheadColor34("#5F6D7E")
+      setInput("white")
+      setMode("light")
+
+    }
+    
+  }
   return (
     <>
       {/* <Login /> */}
       {/* <Login2 /> */}
-      <LastLogin/>
+      <LastLogin head_Bottom={head} mode={Dark} onboard={onboard} input={input} inputColor={inputColor} label={label} headColor={headColor} headColor34={headColor34} night={mode}/> 
     </>
   );
 }
--- src/components/LastLogin.js
+++ src/components/LastLogin.js
@@ -6,76 +6,84 @@ import Done from '../logo/Done.png'
 import Personal from '../logo/Personal.png'
 import Lookscout from '../logo/Lookscout.png'
 import Icon from '../logo/Icon.png'
+import one from '../logo/1.png'
+import two from '../logo/2.png'
+import three from '../logo/3.png'
+import four from '../logo/4.png'
+import LookscoutDark from '../logo/LookscoutDark.png'
 
 
-function LastLogin() {
+function LastLogin(props) {
   return (
-   <>
-    <body>
-    <div className="logo-box">
-      <img src={Lookscout}  alt="Logo" id='Company-logo' />
-    </div>
-    <section className="container">
-      <div className="Head">
-        <div className="logo">
-          <img className='Head-logo' src={Account}  alt="Account" />
-          <span>Account</span>
+    <>
+      <body style={{ backgroundColor: props.onboard }}>
+        <div className="logo-box">
+          <img src={props.night !== "Dark" ? Lookscout : LookscoutDark} alt="Logo" id='Company-logo' />
         </div>
-        <div className="logo">
-          <img className='Head-logo' src={Personal} alt="Personal" />
-          <span>Personal</span>
-        </div>
-        <div className="logo">
-          <img className='Head-logo' src={Billing} alt="Billing" />
-          <span>Billing</span>
-        </div>
-        <div className="logo">
-          <img className='Head-logo' src={Done}  alt="Done" />
-          <span>Done</span>
-        </div>
-      </div>
-  
-      <form action="#" className="form">
-        <div className="input-box">
-          <label>Name</label>
-          <input type="text" placeholder="Enter full name" required />
-        </div>
-  
-        <div className="input-box">
-          <label>Email</label>
-          <input type="text" placeholder="Enter email address" required />
-          <span className='info'>Please input real email address</span>
-        </div>
-        <div className="input-box address">
-          <div className="column">
-            <div>
-              <label> Password</label>
-              <input type="text" placeholder="Enter password" required />
-              <span className='info'>Please Enter your password</span>
+        <section className="container" style={{ border: props.onboard }}>
+          <div className="Head" style={{ backgroundColor: props.head_Bottom }}>
+            <div className="logo">
+              <img className='Head-logo' src={props.night !== "Dark" ? Account : one} alt="Account" />
+              <span style={{ color: props.headColor }}>Account</span>
             </div>
-            <div >
-              <label> Password</label>
-              <input type="number" placeholder="Enter confirm password" required />
-              <span className='info'>Passwords need to match</span>
+            <div className="logo">
+              <img className='Head-logo' src={props.night !== "Dark" ? Personal : two} alt="Personal" />
+              <span style={{ color: props.headColor }}>Personal</span>
+            </div>
+            <div className="logo">
+              <img className='Head-logo' src={props.night !== "Dark" ? Billing : three} alt="Billing" />
+              <span style={{ color: props.headColor34 }}>Billing</span>
+            </div>
+            <div className="logo">
+              <img className='Head-logo' src={props.night !== "Dark" ? Done : four} alt="Done" />
+              <span style={{ color: props.headColor34 }}>Done</span>
             </div>
           </div>
-        </div>
-        <div className="check-box">
-          <input type="checkbox" />
-          <label>I accept the terms and Privacy Policy</label>
-        </div>
-      </form>
-      <div className="bottom-box">
-        <div className="button-box">
-          <button>
-            Next
-            <img style={{marginLeft:"10px"}} src={Icon} alt="" />
-          </button>
-        </div>
-      </div>
-    </section>
-  </body>
-   </>
+
+          <form action="#" className="form" style={{ backgroundColor: props.input }}>
+            <div className="input-box">
+              <label style={{ color: props.label }}>Name</label>
+              <input style={{ backgroundColor: props.inputColor, border: props.input }} type="text" placeholder="Enter full name" required />
+            </div>
+
+            <div className="input-box">
+              <label style={{ color: props.label }}>Email</label>
+              <input style={{ backgroundColor: props.inputColor, border: props.input }} type="text" placeholder="Enter email address" required />
+              <span className='info'>Please input real email address</span>
+            </div>
+            <div className="input-box address">
+              <div className="column">
+                <div>
+                  <label style={{ color: props.label }}> Password</label>
+                  <input style={{ backgroundColor: props.inputColor, border: props.input }} type="text" placeholder="Enter password" required />
+                  <span className='info'>Please Enter your password</span>
+                </div>
+                <div >
+                  <label style={{ color: props.label }}> Confirm Password</label>
+                  <input style={{ backgroundColor: props.inputColor, border: props.input }} type="number" placeholder="Enter confirm password" required />
+                  <span className='info'>Passwords need to match</span>
+                </div>
+              </div>
+            </div>
+            <div className="check-box">
+              <input type="checkbox" />
+              <label style={{ color: props.label }}>I accept the terms and Privacy Policy</label>
+            </div>
+          </form>
+          <div className="bottom-box" style={{ backgroundColor: props.head_Bottom }}>
+            <div className="button-box">
+              <button>
+                Next
+                <img style={{ marginLeft: "10px" }} src={Icon} alt="" />
+              </button>
+            </div>
+          </div>
+        </section>
+      </body>
+      <button onClick={props.mode}>
+        Dark Mode
+      </button>
+    </>
   )
 }
 
